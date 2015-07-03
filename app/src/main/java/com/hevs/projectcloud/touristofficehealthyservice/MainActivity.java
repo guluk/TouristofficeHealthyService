2abdac2d44abed4016bb7139f1d5a592d98ac2e1
gavalni <abbe.gwenaelle@hotmail.com>
2015-07-03 16:50:49 +0200

    add module appengine

--- app/src/main/java/com/hevs/projectcloud/touristofficehealthyservice/MainActivity.java
+++ app/src/main/java/com/hevs/projectcloud/touristofficehealthyservice/MainActivity.java
@@ -4,9 +4,10 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.app.Activity;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
