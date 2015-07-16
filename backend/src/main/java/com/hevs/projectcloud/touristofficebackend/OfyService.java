6b790af00676e6173008493d10e57020bb2779a8
Lukas Germann <lukas.germann@students.hevs.ch>
2015-07-16 13:55:28 +0200

    #6 Objectify service class, used to persist objects

--- backend/src/main/java/com/hevs/projectcloud/touristofficebackend/OfyService.java
+++ backend/src/main/java/com/hevs/projectcloud/touristofficebackend/OfyService.java
@@ -3,15 +3,47 @@ package com.hevs.projectcloud.touristofficebackend;
 import com.googlecode.objectify.Objectify;
 import com.googlecode.objectify.ObjectifyFactory;
 import com.googlecode.objectify.ObjectifyService;
+import com.hevs.projectcloud.touristofficebackend.models.Activity;
+import com.hevs.projectcloud.touristofficebackend.models.Answer;
+import com.hevs.projectcloud.touristofficebackend.models.Category;
+import com.hevs.projectcloud.touristofficebackend.models.Feedback;
+import com.hevs.projectcloud.touristofficebackend.models.Language;
+import com.hevs.projectcloud.touristofficebackend.models.Locale;
+import com.hevs.projectcloud.touristofficebackend.models.Possibility;
+import com.hevs.projectcloud.touristofficebackend.models.Question;
+import com.hevs.projectcloud.touristofficebackend.models.Questionnaire;
+import com.hevs.projectcloud.touristofficebackend.models.Recommendation;
 
 /**
- * Objectify service wrapper so we can statically register our persistence classes
+ *
+ * Created by lukas_000 on 11.07.2015.
+ * Refering to Google's tutorial on
+ * https://cloud.google.com/solutions/mobile/
+ * how-to-build-mobile-app-with-app-engine-backend-tutorial/
+ *
+ * Objectify service wrapper so we can statically register our persistence
+ * classes.
  * More on Objectify here : https://code.google.com/p/objectify-appengine/
  */
 public class OfyService {
 
+    /**
+     * Default constructor, never called.
+     */
+    private OfyService() { }
+
     static {
         ObjectifyService.register(RegistrationRecord.class);
+        factory().register(Activity.class);
+        factory().register(Answer.class);
+        factory().register(Category.class);
+        factory().register(Feedback.class);
+        factory().register(Language.class);
+        factory().register(Locale.class);
+        factory().register(Possibility.class);
+        factory().register(Question.class);
+        factory().register(Questionnaire.class);
+        factory().register(Recommendation.class);
     }
 
     public static Objectify ofy() {
