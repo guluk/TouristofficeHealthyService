5a0ccba4e20d21162d837114e038ec6125c4afa1
Samuel Dorsaz <samuel@micheldorsaz.ch>
2015-08-28 13:10:36 +0200

    Ajout du choix de la langue sur l'écran d'accueil

--- app/src/main/java/com/hevs/projectcloud/touristofficehealthyservice/WelcomeActivity.java
+++ app/src/main/java/com/hevs/projectcloud/touristofficehealthyservice/WelcomeActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 
 public class WelcomeActivity extends Activity {
@@ -13,6 +15,17 @@ public class WelcomeActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        // Get spinner instance
+        Spinner languageSpinner = (Spinner) findViewById(R.id.languageSpinner);
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+        R.array.language_array, android.R.layout.simple_spinner_item);
+
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        languageSpinner.setAdapter(adapter);
     }
 
     @Override
