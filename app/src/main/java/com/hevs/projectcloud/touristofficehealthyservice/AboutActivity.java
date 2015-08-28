d12e792c0d3ba71b8de7165a75dc0b19c2a73f64
Samuel Dorsaz <samuel@micheldorsaz.ch>
2015-08-28 21:52:53 +0200

    Android app work merge

---
+++ app/src/main/java/com/hevs/projectcloud/touristofficehealthyservice/AboutActivity.java
@@ -0,0 +1,15 @@
+package com.hevs.projectcloud.touristofficehealthyservice;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by lukas_000 on 28.08.2015.
+ * for Cloud-Project 645-2 HES-SO Valais
+ */
+public class AboutActivity extends Activity {
+    protected void onCreate(Bundle b){
+        super.onCreate(b);
+        setContentView(R.layout.activity_about);
+    }
+}
--- app/src/main/java/com/hevs/projectcloud/touristofficehealthyservice/WelcomeActivity.java
+++ app/src/main/java/com/hevs/projectcloud/touristofficehealthyservice/WelcomeActivity.java
@@ -1,9 +1,13 @@
 package com.hevs.projectcloud.touristofficehealthyservice;
 
+import java.util.Locale;
+
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,7 +16,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
-
+/**
+ * WelcomeActivity is the first activity when starting the application.
+ */
 public class WelcomeActivity extends Activity
 {
     Spinner languageSpinner;
@@ -56,14 +62,36 @@ public class WelcomeActivity extends Activity
         if (id == R.id.action_settings) {
             return true;
         }
+        //LG/ Opens the "About-Window"
+        if (id == R.id.action_about) {
+            Intent i = new Intent("android.intent.action.ABOUT");
+            startActivity(i);
+            return true;
+        }
 
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * //LG/ Handles the click events from elements on the activity_welcome.xml
+     * @param view the element which was clicked
+     */
     public void onClick(View view) {
         Intent intent;
 
         switch (view.getId()) {
+            case R.id.button_DE :
+                this.setLocale("de");
+                this.continueToMainActivity();
+                break;
+            case R.id.button_FR :
+                this.setLocale("fr");
+                this.continueToMainActivity();
+                break;
+            case R.id.button_EN :
+                this.setLocale("en");
+                this.continueToMainActivity();
+                break;
             case R.id.newButton:
                 intent = new Intent(this, QuestionnaireActivity.class);
                 startActivity(intent);
@@ -71,5 +99,31 @@ public class WelcomeActivity extends Activity
                 intent = new Intent(this, QuestionnaireListActivity.class);
                 startActivity(intent);
         }
+    }
+
+    private void continueToMainActivity() {
+        Intent mainIntent = new Intent(WelcomeActivity.this, MainActivity.class);
+        startActivity(mainIntent);
+    }
+
+    /**
+     * //LG/ Change language and refreshes the window
+     * @param localeCode the locale code of the language which was chosen
+     */
+    private void setLocale(String localeCode) {
+        SharedPreferences sharedPref = getSharedPreferences("language", MODE_PRIVATE);
+
+        Locale locale = new Locale(localeCode);
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.locale = locale;
+        getBaseContext().getResources().updateConfiguration(config,getBaseContext().getResources().getDisplayMetrics());
+
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString("language", localeCode);
+        editor.commit();
+
+        Intent refreshIntent = new Intent(this, WelcomeActivity.class);
+        startActivity(refreshIntent);
     }
 }
