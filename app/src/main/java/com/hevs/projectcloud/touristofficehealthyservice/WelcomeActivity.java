9a5eb5d15ce2483618c7b6830843f175e05b2f81
Samuel Dorsaz <samuel@micheldorsaz.ch>
2015-08-28 14:27:45 +0200

    Add buttons to load Questionnaires activities

--- app/src/main/java/com/hevs/projectcloud/touristofficehealthyservice/WelcomeActivity.java
+++ app/src/main/java/com/hevs/projectcloud/touristofficehealthyservice/WelcomeActivity.java
@@ -1,23 +1,33 @@
 package com.hevs.projectcloud.touristofficehealthyservice;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 
-public class WelcomeActivity extends Activity {
+public class WelcomeActivity extends Activity
+{
+    Spinner languageSpinner;
+    Button  newButton;
+    Button  archivedButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
-        // Get spinner instance
-        Spinner languageSpinner = (Spinner) findViewById(R.id.languageSpinner);
+        // Get layout objects instance
+        languageSpinner = (Spinner) findViewById(R.id.languageSpinner);
+        newButton       = (Button)  findViewById(R.id.archivedButton);
+        archivedButton  = (Button)  findViewById(R.id.newButton);
 
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
@@ -48,5 +58,18 @@ public class WelcomeActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onClick(View view) {
+        Intent intent;
+
+        switch (view.getId()) {
+            case R.id.newButton:
+                intent = new Intent(this, QuestionnaireActivity.class);
+                startActivity(intent);
+            case R.id.archivedButton:
+                intent = new Intent(this, QuestionnaireListActivity.class);
+                startActivity(intent);
+        }
     }
 }
